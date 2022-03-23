5d5b2a5641e8449c17286a5e456a6dea3ed8a1e9
Anisur <anisur.int@orangetoolz.com>
2022-03-23 21:21:13 +0600

    home item done

---
+++ app/src/main/java/com/example/touristpark/view/HomeFragment.java
@@ -0,0 +1,46 @@
+package com.example.touristpark.view;
+
+import android.database.DatabaseUtils;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.touristpark.R;
+import com.example.touristpark.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+    private FragmentHomeBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater,container,false);
+       return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.home_menu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
